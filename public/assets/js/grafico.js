9bffdca98cd17182ddec5841202dd863e335d0dd
pedro henrique <ph8464803@gmail.com>
2020-07-13 18:42:04 -0300

    ajuuste na pagina

---
+++ public/assets/js/grafico.js
@@ -0,0 +1,33 @@
+google.charts.load("current", { packages: ["bar"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["", "", ""],
+    ["jan", 1000, 400],
+    ["fev", 1170, 460],
+    ["mar", 660, 1120],
+    ["abr", 1030, 540],
+    ["maio", 1030, 540],
+    ["jun", 1030, 540],
+    ["jul", 1030, 540],
+    ["ago", 1030, 540],
+    ["set", 1030, 540],
+    ["out", 1030, 540],
+    ["nov", 1030, 540],
+    ["dez", 1030, 540]
+  ]);
+
+  var options = {
+    // chart: {
+    //   title: "Company Performance",
+    //   subtitle: "Sales, Expenses, and Profit: 2014-2017"
+    // }
+  };
+
+  var chart = new google.charts.Bar(
+    document.getElementById("columnchart_material")
+  );
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
